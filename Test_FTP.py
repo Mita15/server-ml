070a6a141093951e25ee140062740015b41f80c2
cgabriel3 <christian.gabriel@binus.ac.id>
2021-12-13 17:35:41 +0700

    FTP Test

--- Test_FTP.py
+++ Test_FTP.py
@@ -4,10 +4,17 @@ host = '34.125.139.205'
 user = 'sftp_tvx'
 password = '<L8m\hEg'
 
-with FTP(host) as ftp:
-    ftp.login(user=user, passwd=password)
-    ftp.cwd('/var/www/tvextract-skripsi/uploads/tmp/')
-    file_name = '41-61b1b4adda1f7.pdf'
-    filepath = open('/var/html/server-ml/samples/41-61b1b4adda1f7.pdf', 'rb')
-    ftp.storbinary('STOR ' + file_name   , filepath)
-    ftp.quit()
+def jalanin_ini():
+    with FTP(host) as ftp:
+        ftp.login(user=user, passwd=password)
+        print('Login success')
+        ftp.cwd('/var/www/tvextract-skripsi/uploads/tmp/')
+        print('Change directory success')
+        file_name = '41-61b1b4adda1f7.pdf'
+        filepath = open('/var/html/server-ml/samples/41-61b1b4adda1f7.pdf', 'rb')
+        print('Opening File')
+        ftp.storbinary('STOR ' + file_name   , filepath)
+        print('Transfer is successful')
+        ftp.quit()
+
+jalanin_ini()
