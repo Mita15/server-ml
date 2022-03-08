38cae57ad4f514913ffe42778b613226886eb5c4
cgabriel3 <92414277+cgabriel3@users.noreply.github.com>
2022-03-08 12:58:16 +0700

    Update tvxt_engine.py

--- tvxt_engine.py
+++ tvxt_engine.py
@@ -35,27 +35,27 @@ def tvxt_engine():
 
     if request.method=="POST":
         command = request.form.get("function")
-        if command == "bca":
-            filenames = request.form.get("filename")
-            url = URL_PDF_FRONTEND + str(filenames) #get pdf from php side
-            filepaths = URL_PDF_BACKEND + str(filenames) #save pdf to python side
-            response = requests.get(url)
-            with open(filepaths,"wb") as f:
-                f.write(response.content)
-            return 'receive'
-        
-       if command == "PROCESS_ID_BCA_TYPE_1":
-			filenames = request.form.get("filename")
-			url = URL_PDF_FRONTEND + str(filenames) #get pdf from php side
-			filepaths = URL_PDF_BACKEND + str(filenames) #save pdf to python side
-			response = requests.get(url)
-			with open(filepaths,"wb") as f:
-				f.write(response.content)
-			print("Enter PROCESS_ID_BCA_TYPE_1")
-			cmd = ["python3", "ID_BCA_1_FUNCTIONS.py"]  ##linux
-			cmd.append(filepaths)
-			PrintLog("API Success",filenames)
-			callrunning = subprocess.Popen(cmd)
+#         if command == "bca":
+#             filenames = request.form.get("filename")
+#             url = URL_PDF_FRONTEND + str(filenames) #get pdf from php side
+#             filepaths = URL_PDF_BACKEND + str(filenames) #save pdf to python side
+#             response = requests.get(url)
+#             with open(filepaths,"wb") as f:
+#                 f.write(response.content)
+#             return 'receive'
+
+	if command == "bca":
+		filenames = request.form.get("filename")
+		url = URL_PDF_FRONTEND + str(filenames) #get pdf from php side
+		filepaths = URL_PDF_BACKEND + str(filenames) #save pdf to python side
+		response = requests.get(url)
+		with open(filepaths,"wb") as f:
+			f.write(response.content)
+		print("Enter PROCESS_ID_BCA_TYPE_1")
+		cmd = ["python3", "ID_BCA_1_FUNCTIONS.py"]  ##linux
+		cmd.append(filepaths)
+		PrintLog("API Success",filenames)
+		callrunning = subprocess.Popen(cmd)
     
 
     return jsonify(True)
